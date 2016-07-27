d97ccf36406c0bfbcc08fa8f4e0c990e3cb82001
Hong <kim135792@naver.com>
2016-07-27 10:31:06 +0900

    한글 패치용

--- src/main/java/org/singlelife/controller/ParcelController.java
+++ src/main/java/org/singlelife/controller/ParcelController.java
@@ -16,22 +16,22 @@ public class ParcelController {
 	
 	@RequestMapping("/parcel")
 	public ResponseEntity<String> parcel(String code, String invoice) throws Exception {
-		String url = "http://tracking.sweettracker.net/tracking"; //��鍮���URL
-        String key = "FYruI1RApROgoyBW9SDmlQ"; //��泥��댁�� 諛��� ��
+		String url = "http://tracking.sweettracker.net/tracking"; // 서비스  URL
+        String key = "FYruI1RApROgoyBW9SDmlQ"; // 신청해서 받은 KEY
         String furl = String.format("%s/?t_key=%s&t_code=%s&t_invoice=%s", url, key, code, invoice);
         
         URL obj = new URL(furl);
         HttpURLConnection httpConn = (HttpURLConnection) obj.openConnection();
 
-        // ���〓갑�� GET 吏���
+        // 전송방식 GET 지정
         httpConn .setRequestMethod("GET");
 
-        // ��泥��ㅻ�� 異�媛�
+        // 요청헤더 추가
         httpConn .setRequestProperty("User-Agent", "Mozilla/5.0");
         httpConn .setRequestProperty("Accept", "application/json");
         
         int responseCode = httpConn.getResponseCode();
-        // ���듭��� ����(�����몄��� 200)
+        // 응답코드 확인(정상호출 시 200)
         System.out.println("Response Code : " + responseCode);
         
         BufferedReader in = new BufferedReader(new InputStreamReader(httpConn.getInputStream(),"UTF-8"));
@@ -45,18 +45,9 @@ public class ParcelController {
 		HttpHeaders responseHeaders = new HttpHeaders();
 	    responseHeaders.add("Content-Type","text/html;charset=UTF-8");
         ResponseEntity<String> entity = new ResponseEntity<>(response.toString(),responseHeaders,HttpStatus.OK);
-        //寃곌낵 ����
-        
-//        String result = response.toString();
-//        CharBuffer cbuffer = CharBuffer.wrap(new String(result.getBytes(), "UTF-8").toCharArray());
-//        Charset utf8 = Charset.forName("EUC-KR");
-//        ByteBuffer bbuffer = utf8.encode(cbuffer);
-//        
-//        String tmp = new String(bbuffer.array());
-//        
-//        System.out.println(tmp);
-        System.out.println(response.toString());
-        String result = response.toString();
+
+        // 결과 확인
+        System.out.println(entity);
 		return entity;
 	}
 }
